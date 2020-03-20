f98fb929902b909438c00df5ac113bc8a369d8cc
Fernandoge <fernando.gp101@gmail.com>
2020-03-20 20:28:36 -0300

    Added Article view controller to add selected Article in database after performing the segue

---
+++ HackerNews Articles/HackerNews Articles/Controller/ArticleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleViewController.swift
+//  HackerNews Articles
+//
+//  Created by Fernando Garcia on 20-03-20.
+//  Copyright © 2020 Fernando Garcia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleViewController: UIViewController {
+    var articleURL = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(articleURL)
+    }
+    
+}
--- HackerNews Articles/HackerNews Articles/Controller/HackerNewsArticlesViewController.swift
+++ HackerNews Articles/HackerNews Articles/Controller/HackerNewsArticlesViewController.swift
@@ -7,25 +7,38 @@
 //
 
 import UIKit
+import RealmSwift
 
 class HackerNewsArticlesViewController: UITableViewController {
 
     @IBOutlet weak var articlesRefresher: UIRefreshControl!
     
+    let realm = try! Realm()
     var articlesArray: [Article] = []
-    var articleURL = ""
+    var selectedArticleURL = ""
     var hackerNewsManager = HackerNewsManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hackerNewsManager.delegate = self
         hackerNewsManager.fetchArticles()
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL!)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "showArticle") {
+            if let destiny = segue.destination as? ArticleViewController {
+                destiny.articleURL = selectedArticleURL
+            }
+        }
     }
     
     @IBAction func refresh(_ sender: UIRefreshControl) {
         hackerNewsManager.fetchArticles()
     }
     
+    //MARK: -- Table view methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articlesArray.count
@@ -35,6 +48,25 @@ class HackerNewsArticlesViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath)
         cell.textLabel?.text = articlesArray[indexPath.row].name
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedArticle = articlesArray[indexPath.row]
+        selectedArticleURL = selectedArticle.articleURL
+        saveDownloadedArticle(article: selectedArticle)
+        performSegue(withIdentifier: "showArticle", sender: self)
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveDownloadedArticle(article: Article) {
+        do {
+            try realm.write {
+                realm.add(article, update: .modified)
+            }
+        } catch {
+            print ("Error saving downloaded article \(error)")
+        }
     }
 }
 
