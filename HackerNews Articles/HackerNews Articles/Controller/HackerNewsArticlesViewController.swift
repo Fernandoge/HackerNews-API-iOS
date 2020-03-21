0139f1430101f5c4f7081a965674c62354f6552d
Fernandoge <fernando.gp101@gmail.com>
2020-03-20 21:13:45 -0300

    Added two sections, Recent Articles and Downloaded Articles for the table view

--- HackerNews Articles/HackerNews Articles/Controller/HackerNewsArticlesViewController.swift
+++ HackerNews Articles/HackerNews Articles/Controller/HackerNewsArticlesViewController.swift
@@ -14,7 +14,7 @@ class HackerNewsArticlesViewController: UITableViewController {
     @IBOutlet weak var articlesRefresher: UIRefreshControl!
     
     let realm = try! Realm()
-    var articlesArray: [Article] = []
+    var articlesArray: [[Article]] = []
     var selectedArticleURL = ""
     var hackerNewsManager = HackerNewsManager()
     
@@ -22,8 +22,15 @@ class HackerNewsArticlesViewController: UITableViewController {
         super.viewDidLoad()
         hackerNewsManager.delegate = self
         hackerNewsManager.fetchArticles()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //Reload downloaded articles
+        if articlesArray.count > 0 {
+            let downloadedArticles = getDownloadedArticles()
+            loadArticles(recentArticles: articlesArray[0], downloadedArticles: downloadedArticles)
+        }
         
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -40,18 +47,35 @@ class HackerNewsArticlesViewController: UITableViewController {
     
     //MARK: -- Table view methods
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let headerTitles = ["Recent Articles", "Downloaded Articles"]
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section < headerTitles.count {
+            return headerTitles[section]
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView
+        header.textLabel?.textAlignment = .center
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return articlesArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articlesArray[section].count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath)
-        cell.textLabel?.text = articlesArray[indexPath.row].name
+        cell.textLabel?.text = articlesArray[indexPath.section][indexPath.row].name
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedArticle = articlesArray[indexPath.row]
+        let selectedArticle = articlesArray[indexPath.section][indexPath.row]
         selectedArticleURL = selectedArticle.articleURL
         saveDownloadedArticle(article: selectedArticle)
         performSegue(withIdentifier: "showArticle", sender: self)
@@ -68,17 +92,30 @@ class HackerNewsArticlesViewController: UITableViewController {
             print ("Error saving downloaded article \(error)")
         }
     }
+    
+    func getDownloadedArticles() -> [Article]{
+        let downloadedArticles = realm.objects(Article.self)
+        let downloadedArticlesArray = Array(downloadedArticles.reversed())
+        return downloadedArticlesArray
+    }
+    
+    func loadArticles(recentArticles: [Article], downloadedArticles: [Article]) {
+        articlesArray = []
+        articlesArray.append(recentArticles)
+        articlesArray.append(downloadedArticles)
+        DispatchQueue.main.async {
+            self.articlesRefresher.endRefreshing()
+            self.tableView.reloadData()
+        }
+    }
 }
 
 //MARK: - HackerNewsManagerDelegate
 
 extension HackerNewsArticlesViewController: HackerNewsManagerDelegate {
     func didUpdateArticles(_ hackerNewsManager: HackerNewsManager, articles: [Article]) {
-        DispatchQueue.main.async {
-            self.articlesArray = articles
-            self.articlesRefresher.endRefreshing()
-            self.tableView.reloadData()
-        }
+        let downloadedArticles = getDownloadedArticles()
+        loadArticles(recentArticles: articles, downloadedArticles: downloadedArticles)
     }
     
     func didFailedWithError(error: Error) {
